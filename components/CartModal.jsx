fa8492438646da2d914176ada3cefb0c95863abc
Protik111 <rafiurprotik111@gmail.com>
2022-03-11 09:58:11 +0600

    cart style added

--- components/CartModal.jsx
+++ components/CartModal.jsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import AOS from 'aos';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { ImCross } from 'react-icons/im';
 import 'aos/dist/aos.css';
+import styles from '../styles/CartModal.module.css';
+import { StoreContext } from '../utils/context';
+import CartStyle from './CartStyle';
 
 const CartModal = ({ showCart, setShowCart }) => {
+    const { state, dispatch } = useContext(StoreContext);
+    const { cart } = state;
     const modalRef = useRef();
 
     const closeModal = (e) => {
-        if(e.target === modalRef.current){
+        if (e.target === modalRef.current) {
             setShowCart(false);
         }
+    }
+    const handleCancel = () => {
+        setShowCart(false)
     }
     useEffect(() => {
         AOS.init({
@@ -21,7 +31,33 @@ const CartModal = ({ showCart, setShowCart }) => {
     return (
         <div data-aos="zoom-in-left" className="cartModal" ref={modalRef} onClick={closeModal}>
             <div className="modalContainer">
-                <h2>CartModal</h2>
+                <div className={`${styles.header} d-flex px-5 py-4`}>
+                    <div className='d-flex justify-conten-center px-3'>
+                        <span><AiOutlineShoppingCart></AiOutlineShoppingCart></span>
+                        <h5 className="ms-1">Shopping Cart</h5>
+                    </div>
+                    <div onClick={handleCancel}>
+                        <span className={styles.cancel}><ImCross style={{ fontSize: '1rem', color: 'red', cursor: 'pointer' }}></ImCross></span>
+                    </div>
+                </div>
+                <div>
+                    {
+                        cart.map(item => <CartStyle item={item}></CartStyle>)
+                    }
+                </div>
+                <div className={`${styles.checkout} p-4`}>
+                    {cart.length > 0 ? (
+                        <>
+                            <div className="px-5">
+                                <h6 className="mt-2">Proceed To Checkout</h6>
+                            </div>
+                            <div className={`${styles.price} px-3`}>
+                                <h6 className='mt-2'>$26.00</h6>
+                            </div></>
+                    ) : (<div className="">
+                        <h6 className="">There is no Product in the cart.</h6>
+                    </div>)}
+                </div>
             </div>
         </div>
     );
---
+++ components/CartStyle.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { MdDelete } from 'react-icons/md';
+import styles from '../styles/CartStyle.module.css';
+
+const CartStyle = ({ item }) => {
+    return (
+        <div className={styles.container}>
+            <div className="ms-md-3">
+                <Image className={styles.cartImage} width={70} height={70} src={item.image}></Image>
+            </div>
+            <div className={`${styles.price} mx-md-5 my-md-2`}>
+                <h6>{item.name}</h6>
+                <p>Item Price: ${item.price}</p>
+                <h5>Total : $50</h5>
+            </div>
+            <div className={styles.delete}>
+                <span><MdDelete style={{fontSize: '30px', color: 'red'}}></MdDelete></span>
+            </div>
+        </div>
+    );
+};
+
+export default CartStyle;
